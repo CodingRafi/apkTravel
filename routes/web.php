9b6a0d320f8b63809558d64bf0a4e78a1d1af3d9
CodingRafi <codingmuhamadrafiprastya@gmail.com>
2021-12-30 13:00:03 +0700

    Menambahkan Template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index']);
