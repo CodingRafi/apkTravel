33a6f5ebc899c8e0bd8725f48323b883d5669e95
enjoyyourcreditcard <joywidiwibowo@gmail.com>
2022-01-03 13:10:41 +0700

    nyoba css pake display grid

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,27 +18,73 @@
     {{-- @yield('container') --}}
 
     <header>
-
+        <div class="logo-frame">
+          <h1>Kota Depok</h1>
+        </div>
+        <div class="running">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, autem praesentium! A, quaerat iure. Incidunt
+              possimus odit doloremque molestias libero.</p>
+        </div>
     </header>
 
     <div class="grid-home">
-      <div class="grid-home item">
-        <p>sambutan</p>
-      </div>
-      <div class="grid-home item">
-        <p>cuacah</p>
-      </div>
-      <div class="grid-home item">
-        <p>peta kota depok</p>
-      </div>
-      <div class="grid-home item">
-        <p>kontak serfis</p>
-      </div>
-      <div class="grid-home item">
-        <p>berita</p>
-      </div>
+        <div class="grid-home-sambutan">
+            <img src="images/home-screen/video.jpg">
+        </div>
+        <div class="grid-home-peta">
+          <h1>Peta Kota Depok</h1>
+          <img src="images/home-screen/w01.jpg">
+        </div>
+        <div class="grid-home-berita">
+          <h1>Berita</h1>
+          <div class="berita-item">
+            <img src="images/home-screen/berita.jpg">
+            <div>
+              <h1>Komunitas Bank Sampah Kecamatan</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ducimus unde quasi atque saepe minus doloribus rerum soluta mollitia ullam.</p>
+            </div>
+          </div>
+          <div class="berita-item">
+            <img src="images/home-screen/berita.jpg">
+            <div>
+              <h1>Komunitas Bank Sampah Kecamatan</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ducimus unde quasi atque saepe minus doloribus rerum soluta mollitia ullam.</p>
+            </div>
+          </div>
+          <div class="berita-item">
+            <img src="images/home-screen/berita.jpg">
+            <div>
+              <h1>Komunitas Bank Sampah Kecamatan</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ducimus unde quasi atque saepe minus doloribus rerum soluta mollitia ullam.</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-home-cuaca">
+          <h1>07:30</h1>
+          <p>22 Desember 2021</p>
+          <h1>20°C</h1>
+          <img src="images/home-screen/rainy-day.png">
+        </div>
+        <div class="grid-home-kontak">
+          <h1>Contact Center</h1>
+          <h1>021 - 8212 - 210</h1>
+          <h1>Customer Center</h1>
+          <h1>021 - 8212 - 210</h1>
+        </div>
     </div>
-    
+    <div class="menu">
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/hotel">Hotel</a></li>
+        <li><a href="/destinasi">Destinasi</a></li>
+        <li><a href="/makanan">Makanan</a></li>
+        <li><a href="/travel">Travel</a></li>
+        <li><a href="/ekraf">Ekraf</a></li>
+      </ul>
+    </div>
+
+
+
     {{-- <div class="footer">
       <ul>
         <li><a href="/home">home</a></li>
