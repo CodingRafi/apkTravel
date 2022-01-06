51aec16594a25fc1e57ed51ab4b4837981b7c786
enjoyyourcreditcard <joywidiwibowo@gmail.com>
2022-01-06 18:01:25 +0700

    memperbaiki layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
@@ -16,84 +15,68 @@
 </head>
 
 <body>
-    <header class="flex">
-        <div class="logo-frame flex">
-            <img src="images/home-screen/logo.png" alt="logo" style="height: 4rem; object-fit: cover;">
-            <h2 style="white-space: nowrap">Kota Depok</h2>
+    <div class="topbar">
+        <div class="topbar-head flex">
+            <img src="images/home-screen/logo-depok.png" alt="logo-kota-depok">
+            <h2>Kota Depok</h2>
         </div>
-        <div class="marquee-frame">
-            <marquee behavior="scroll" direction="left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, praesentium voluptates, ullam, libero ipsa debitis reiciendis nam magnam sapiente eligendi ab amet fugit commodi iure. Nulla doloremque dicta voluptas eaque nihil magni mollitia animi natus praesentium. Quae, modi! Tempore esse ex soluta voluptates. Sint, quia totam sapiente ipsum incidunt, eaque voluptate dolor cupiditate fuga quaerat veniam excepturi quae delectus vero quo distinctio quisquam hic, sed eligendi rerum molestias dignissimos vitae.</marquee>
-        </div>
-    </header>
-    <div class="content grid">
-        <div class="sambutan-frame">
-            <div class="sambutan-video">
-                <img src="images/home-screen/video.jpg">
-            </div>
-        </div>
-        <div class="peta-frame flex">
-            <div class="peta-img">
-                <img src="images/home-screen/depok-map.jpg">
-            </div>
-            <div class="peta-body">
-                <h2>Peta Kota Depok</h2>
-                <p>Kecamatan Beji</p>
-                <ul class="legenda">
-                    <li>Situ Jatijajar</li>
-                    <li>Margo City</li>
-                    <li>Stasiun Depok Baru</li>
-                </ul>
-            </div>
-        </div>
-        <div class="berita-frame">
-            <div class="berita-item flex">
-                <div>
-                    <div class="berita-img">
-
-                    </div>
-                </div>
-                <div>
-                    <b>Komunitas Bank Sampah Kecamatan Sukmajaya Hasilkan Berbagai Produk Daur Ulang</b>
-                </div>
-            </div>
-        </div>
-        <div class="cuaca-frame flex">
-            <div class="cuaca-head">
-                <center>
-                    <h1>07:00</h1>
-                    <p>22 Desember 2020</p>
-                </center>
-            </div>
-            <div class="flex cuaca-body">
-                <h1>20°C</h1>
-                <div class="cuaca-icon">
-
-                </div>
-            </div>
-        </div>
-        <div class="kontak-frame flex">
-            <div>
-                <center>
-                    <h2>Contact Center</h2>
-                    <h1>021 - 8212 - 210</h1>
-                </center>
-            </div>
-            <div>
-                <center>
-                    <h2>Customer Center</h2>
-                    <h1>021 - 8212 - 210</h1>
-                </center>
-            </div>
+        <div class="topbar-body flex">
+            <marquee behavior="scroll" direction="left">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perferendis ipsa optio odit, a autem repudiandae totam sapiente debitis illo illum doloribus laborum
+                maxime, quia error soluta, harum recusandae obcaecati id iusto voluptas culpa eius magnam quod.
+                Voluptatibus dignissimos veritatis quo voluptates! Assumenda nobis cupiditate nulla maiores dolor maxime
+                possimus. Deleniti officia illo molestiae necessitatibus, numquam asperiores laudantium repellendus
+                commodi unde, dolores tenetur porro cupiditate incidunt id libero maxime, atque sed.
+            </marquee>
         </div>
     </div>
-    <ul class="footer flex">
-        <li><a href="/home">Home</a></li>
-        <li><a href="#">Hotel</a></li>
-        <li><a href="#">Destinasi</a></li>
-        <li><a href="#">Makanan</a></li>
-        <li><a href="#">Travel</a></li>
-        <li><a href="#">Oleh-Oleh</a></li>
-    </ul>
+    <div class="main">
+        <div class="sambutan-frame"></div>
+        <div class="peta-frame"></div>
+        <div class="berita-frame"></div>
+        <div class="cuaca-frame"></div>
+        <div class="kontak-frame"></div>
+    </div>
+    <div class="menu">
+        <ul class="menu-frame flex">
+            <li>
+                <a href="/home" class="menu-icon flex">
+                  <img src="icon/home-screen/rumah.svg">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="#" class="menu-icon flex">
+                  <img src="icon/home-screen/hotel.svg">
+                    Hotel
+                </a>
+            </li>
+            <li>
+                <a href="#" class="menu-icon flex">
+                  <img src="icon/home-screen/destinasi.svg">
+                    Destinasi
+                </a>
+            </li>
+            <li>
+                <a href="#" class="menu-icon flex">
+                  <img src="icon/home-screen/makanan.svg">
+                    Makanan
+                </a>
+            </li>
+            <li>
+                <a href="#" class="menu-icon flex">
+                  <img src="icon/home-screen/travel.svg">
+                    Travel
+                </a>
+            </li>
+            <li>
+                <a href="#" class="menu-icon flex">
+                  <img src="icon/home-screen/oleh-oleh.svg">
+                    Oleh-Oleh
+                </a>
+            </li>
+        </ul>
+    </div>
 </body>
 
 </html>
