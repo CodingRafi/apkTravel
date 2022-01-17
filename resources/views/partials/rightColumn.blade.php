2dee6d79b0f4b2120b060e483f93354f3afe7286
enjoyyourcreditcard <joywidiwibowo@gmail.com>
2022-01-17 08:09:11 +0700

    gambar berita dinamis

--- resources/views/partials/rightColumn.blade.php
+++ resources/views/partials/rightColumn.blade.php
@@ -1,17 +1,23 @@
+
+
 <div class="column flex">
     {{-- berita --}}
     <div class="berita-kota-depok">
         <h4>Berita Kota Depok</h4>
     </div>
     <div class="berita-frame">
-        @foreach ($beritas as $data)
+        @for ($i = 0; $i < count($beritas); $i++)
             <div class="card pt-3" style="max-width: 540px;">
-                <img src="images/home-screen/berita.jpg" class="card-img-top" alt="...">
+                @if (count($foto[$i]) > 0)
+                    <img src="{{ $foto[$i][0]->filename }}" class="card-img-top" alt="...">
+                @else
+                    <img src="images/home-screen/berita.jpg" class="card-img-top" alt="...">    
+                @endif
                 <div class="card-body">
-                    <h5 class="card-title">{{ $data->judul }}</h5>
-                    <p class="card-text">{!! $data->body !!}</p>
+                    <h5 class="card-title">{{ $beritas[$i]->judul }}</h5>
+                    <p class="card-text">{!! Str::limit($beritas[$i]->body, 60) !!}</p>
                 </div>
             </div>
-        @endforeach
+        @endfor
     </div>
 </div>
