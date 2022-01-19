be00a856821fea82e02dff058df5127db2f7d89b
enjoyyourcreditcard <joywidiwibowo@gmail.com>
2022-01-19 07:11:58 +0700

    list hotel ke detil hotel

--- resources/views/partials/subColumn.blade.php
+++ resources/views/partials/subColumn.blade.php
@@ -1,11 +1,13 @@
 <div class="column sub flex">
     @foreach ($wibowo as $data)
         <div class="card bg-dark text-white">
-            <img src="images/home-screen/h1c.jpg" class="card-img" alt="...">
-            <div class="card-img-overlay">
-                <h5 class="card-title">{{ $data->nama }}</h5>
-                <p class="card-text">{{ $data->alamat }}</p>
-            </div>
+            <a href="show/{{ $data->slug }}">
+                <img src="images/home-screen/h1c.jpg" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">{{ $data->nama }}</h5>
+                    <p class="card-text">{{ $data->alamat }}</p>
+                </div>
+            </a>
         </div>
     @endforeach
 
