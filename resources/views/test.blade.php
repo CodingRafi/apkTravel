ee16f6c0c995097f7424f40878b664190fd572b9
CodingRafi <codingmuhamadrafiprasetya@gmail.com>
2022-08-15 12:06:25 +0700

    solve

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -526,7 +526,7 @@
         var dd = String(today.getDate()).padStart(2, '0');
         var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
         var yyyy = today.getFullYear();
-e
+
         today = yyyy + '/' + mm + '/' + dd;
 
         var parts = today.split('/');
